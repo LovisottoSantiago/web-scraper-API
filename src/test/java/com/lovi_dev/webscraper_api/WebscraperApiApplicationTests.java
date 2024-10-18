baf1467a2bd0b3b04369ebd1c13cc9a0ed770ab6
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-10-18 11:15:20 -0300

    Spring initializr

---
+++ src/test/java/com/lovi_dev/webscraper_api/WebscraperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovi_dev.webscraper_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebscraperApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
